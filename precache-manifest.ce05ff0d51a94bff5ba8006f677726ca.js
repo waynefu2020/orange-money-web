872ae92d54628f2e184f341a79e20524e891ad6b
DDWayne <652507565@qq.com>
2021-05-31 10:46:55 +0800

    deploy

---
+++ precache-manifest.ce05ff0d51a94bff5ba8006f677726ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "201dd599a827501eda5ca9877cd165e3",
+    "url": "/orange-money-web/index.html"
+  },
+  {
+    "revision": "f087a583235eb9b0d411",
+    "url": "/orange-money-web/static/css/main.0360e6a2.chunk.css"
+  },
+  {
+    "revision": "ecb9d063152689172e3a",
+    "url": "/orange-money-web/static/js/2.2630d556.chunk.js"
+  },
+  {
+    "revision": "fc8b115d4064d6797b6def218d457695",
+    "url": "/orange-money-web/static/js/2.2630d556.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f087a583235eb9b0d411",
+    "url": "/orange-money-web/static/js/main.a2229f07.chunk.js"
+  },
+  {
+    "revision": "4d292451aabebdb9fb2c",
+    "url": "/orange-money-web/static/js/runtime-main.65f46eee.js"
+  }
+]);
